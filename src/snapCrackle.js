2dd09b9cf4d8ec7fe4c6f8d469913ed0ba691757
Igor <igorpiresfelipe@hotmail.com>
2021-10-18 14:32:22 -0300

    Entrega 43 Katas

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,31 @@
 // Seu código vai aqui
+
+function snapCrackle(maxValue){
+    
+    let conjuntoNum = [];
+  
+    for ( let contador=1 ; contador <= maxValue; contador++){
+            
+        if (contador % 5 === 0 && contador % 2 === 1 ){
+            conjuntoNum.push(' SnapCrackle');       
+                        }
+      else  if (contador % 5 === 0){      
+            conjuntoNum.push(' Crackle');    
+                    } 
+
+      if (contador % 2 === 1 ){
+             conjuntoNum.push(' Snap');
+                }
+    
+        if (contador % 2 !== 1 &&  contador % 5 !==0){
+            conjuntoNum.push(contador);       
+            
+             }    
+               
+         
+       } return conjuntoNum;
+  }
+    
+  
+  
+  console.log(snapCrackle(12));
